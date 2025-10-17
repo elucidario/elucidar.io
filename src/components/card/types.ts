dc231044395f458a594bb043e41bde86e3c718b0
hgodinho <ola@hgod.in>
2025-10-17 16:47:34 -0300

    feat: Enhance Card and Deck components with border styles and pattern support

--- src/components/card/types.ts
+++ src/components/card/types.ts
@@ -1,5 +1,7 @@
+import type { pattern } from "@/styles";
+import type { VariantProps } from "class-variance-authority";
 import type { HTMLAttributes, ReactNode } from "react";
 
 export type DeckProps = HTMLAttributes<HTMLUListElement> & {
     items?: ReactNode[];
-};
+} & VariantProps<typeof pattern>;
