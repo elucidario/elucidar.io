b2bb504fa3810c95fb521d19b1bd4381a3b175cd
hgodinho <ola@hgod.in>
2025-10-28 14:38:10 -0300

    refactor: simplify Header component by removing unused imports and motion wrapper

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,26 @@
-import { useRef } from "react";
-import { motion } from "motion/react";
-
 import type { HeaderProps } from "./types";
 import { cn } from "@/utils";
-import { Logo } from "@/components/logo";
-import { Menu } from "../menu";
-import { Line } from "../line";
-import { Spacing } from "../spacing";
-import { Heading } from "../typography";
+import { Logo, Spacing, Menu, Line } from "@/components";
 import { mask, pattern } from "@/styles";
 
 export function Header({ className, ...props }: HeaderProps) {
-    const headingRef = useRef<HTMLDivElement>(null);
-
     return (
-        <motion.header
-            ref={headingRef}
-            className={cn("w-full", className)}
+        <header
+            className={cn(
+                "header",
+                "top-0",
+                "sticky",
+                "flex",
+                "flex-col",
+
+                "z-10",
+
+                "bg-light dark:bg-dark",
+
+                "w-full",
+                "h-fit",
+                className
+            )}
             {...props}
         >
             <div
@@ -37,17 +41,9 @@ export function Header({ className, ...props }: HeaderProps) {
             ></div>
             <Spacing className="h-10" />
             <Menu />
-            <Spacing className="h-16" />
             <Line>
                 <Logo name="elucidar.io" className={cn("h-12", "self-start")} />
             </Line>
-            <Spacing />
-            <Line>
-                <Heading className={cn("font-mono", "text-pink-55")}>
-                    Serviços e produtos tecnológicos para <br /> cultura,
-                    educação e informação.
-                </Heading>
-            </Line>
-        </motion.header>
+        </header>
     );
 }
