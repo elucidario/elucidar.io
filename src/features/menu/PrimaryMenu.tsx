035c714f2b37a9cb37dbf1041c5171f330e7d39c
hgodinho <ola@hgod.in>
2025-11-05 00:10:59 -0300

    feat: add PrimaryMenu component with menu links and export from index

---
+++ src/features/menu/PrimaryMenu.tsx
@@ -0,0 +1,40 @@
+import { Menu, type MenuProps } from "@/components";
+import { linkOptions } from "@tanstack/react-router";
+
+const links: MenuProps["links"] = [
+    {
+        label: "serviços",
+        color: "pink",
+        options: linkOptions({
+            to: "/",
+            hash: "servicos",
+        }),
+    },
+    {
+        label: "produtos",
+        color: "blue",
+        options: linkOptions({
+            to: "/",
+            hash: "produtos",
+        }),
+    },
+    {
+        label: "contato",
+        color: "gray",
+        options: linkOptions({
+            to: "/",
+            hash: "contato",
+        }),
+    },
+    {
+        label: "blog",
+        color: "gray",
+        options: linkOptions({
+            to: "/blog",
+        }),
+    },
+];
+
+export function PrimaryMenu() {
+    return <Menu links={links} />;
+}
