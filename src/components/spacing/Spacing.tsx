e58cacc669b747b68a6c26f324f6faf2b6c254c4
hgodinho <ola@hgod.in>
2025-11-05 21:32:36 -0300

    feat: refactor Spacing component to use GridLine and Cell for improved layout and styling

--- src/components/spacing/Spacing.tsx
+++ src/components/spacing/Spacing.tsx
@@ -1,28 +1,42 @@
 import { cn } from "@/utils";
 import { type VariantProps } from "class-variance-authority";
 import type { HTMLAttributes } from "react";
-import { Line, xLineVariants, yLineVariants } from "../line";
+import { Cell, GridLine, xLineVariants, yLineVariants } from "../line";
+import { pattern as patternStyles } from "@/styles";
 
-type SpacingProps = HTMLAttributes<HTMLDivElement>;
+type SpacingProps = HTMLAttributes<HTMLDivElement> & {
+    pattern?: VariantProps<typeof patternStyles>;
+};
 
 export function Spacing({
     right = true,
     left = true,
     top = false,
     bottom = false,
+    pattern,
     className,
     ...props
 }: SpacingProps &
     VariantProps<typeof xLineVariants> &
     VariantProps<typeof yLineVariants>) {
     return (
-        <Line
-            right={right}
-            left={left}
+        <GridLine
             top={top}
             bottom={bottom}
-            className={cn("min-h-4", "h-4", className)}
+            className={cn(
+                "spacing",
+                "min-h-4",
+                "h-4",
+                pattern && patternStyles(pattern),
+                className
+            )}
             {...props}
-        />
+        >
+            <Cell
+                right={right}
+                left={left}
+                className="col-start-1 col-end-13 flex w-full h-full"
+            />
+        </GridLine>
     );
 }
